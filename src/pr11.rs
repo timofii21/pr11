3d2378e37c4971dd90bfc4f899e7e8f7af130a8c
timofii21 <tkaprizov7777@gmail.com>
2024-10-29 22:26:07 +0200

    1

---
+++ src/pr11.rs
@@ -0,0 +1,24 @@
+fn is_palindrome(x: u32) -> bool {
+    let s = x.to_string();
+    s.chars().eq(s.chars().rev())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let data = [
+            (123, false),
+            (121, true),
+            (1221, true),
+        ];
+
+        data
+            .iter()
+            .for_each(|(n, exp)| {
+                assert_eq!(is_palindrome(*n), *exp);
+            });
+    }
+}
